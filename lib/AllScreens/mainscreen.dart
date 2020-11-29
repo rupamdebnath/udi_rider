c73b455d865bbc41e4d96a43c1bc1146cc9eb34f
rupamdebnath <rdebmail@gmail.com>
2020-11-29 21:05:16 +1100

    Added Navigation for Profile

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -15,6 +15,8 @@ class _MainScreenState extends State<MainScreen>
   Completer<GoogleMapController> _controllerMap = Completer();
   GoogleMapController newGoogleMapController;
 
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
@@ -23,8 +25,57 @@ class _MainScreenState extends State<MainScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text("Main Screen")
+      ),
+      drawer: Container(
+        color: Colors.white,
+        width: 255.0,
+        child: Drawer(
+          child: ListView(
+            children: [
+              Container(
+                height: 165.0,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Row(
+                    children: [
+                      Image.asset("images/user_icon.png", height: 65.0, width: 65.0,),
+                      SizedBox(width: 16.0,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Profile Name", style: TextStyle(fontSize: 16.0, fontFamily: "Brand-Bold"),),
+                          SizedBox(height: 6.0,),
+                          Text("Visit Profile"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              DividerWidget(),
+
+              SizedBox(height: 12.0,),
+
+              //Drawbody controllers
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text("History", style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Visit Profile", style: TextStyle(fontSize: 15.0),),
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text("History", style: TextStyle(fontSize: 15.0),),
+              ),
+            ],
+          )
+        )
       ),
       body: Stack(
         children: [
@@ -38,6 +89,42 @@ class _MainScreenState extends State<MainScreen>
                   newGoogleMapController = controller;
               }
           ),
+
+          //Hamburger button for drawer
+
+          Positioned(
+            top: 45.0,
+            left: 22.0,
+            child: GestureDetector(
+              onTap: ()
+              {
+                  scaffoldKey.currentState.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 6.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(
+                        0.7,
+                        0.7,
+                      ),
+                    ),
+                  ],
+                ),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.menu, color: Colors.black,),
+                  radius: 20.0,
+                ),
+              ),
+            ),
+          ),
+
           Positioned(
             left: 0.0,
             right: 0.0,
